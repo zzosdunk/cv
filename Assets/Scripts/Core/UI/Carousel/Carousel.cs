d038d33d0703944d550d03e631ff2bf9fd71884e
zzosdunk <zzosdunk@gmail.com>
2022-02-03 23:31:51 +0100

    carousel controller

---
+++ Assets/Scripts/Core/UI/Carousel/Carousel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Carousel : MonoBehaviour
+{
+    [SerializeField] private List<Sprite> _currentCarouselCollection = new List<Sprite>();
+    [SerializeField] private List<CarouselItem> _carouselItems = new List<CarouselItem>();
+
+    [SerializeField] private Image _mainImage;
+
+    private void Start()
+    {
+        LoadCarousel();
+    }
+
+    public void LoadCarousel()
+    {
+        for (int i = 0; i < _carouselItems.Count; i++)
+        {
+            _carouselItems[i].SetItem(_currentCarouselCollection[i]);
+        }
+        
+        SetMainImage(_carouselItems[0].GfxRepresentation);
+    }
+    
+    public void CarouselInit(List<Sprite> sprites)
+    {
+        _currentCarouselCollection = sprites;
+    }
+
+    public void SetMainImage(Sprite chosenImage)
+    {
+        _mainImage.sprite = chosenImage;
+    }
+}
---
+++ Assets/Scripts/Core/UI/Carousel/CarouselItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CarouselItem : MonoBehaviour
+{
+    [SerializeField] private Carousel _carouselController;
+    [SerializeField] private Sprite _gfxRepresentation;
+    private Image _imageField;
+    public Sprite GfxRepresentation => _gfxRepresentation;
+
+    private Button _button;
+
+    private void Awake()
+    {
+        _button = GetComponent<Button>();
+        _imageField = GetComponent<Image>();
+        
+        _button.onClick.AddListener(OnClicked);
+    }
+
+    public void SetItem(Sprite referenceImage)
+    {
+        _gfxRepresentation = referenceImage;
+        _imageField.sprite = _gfxRepresentation;
+    }
+
+    void OnClicked()
+    {
+        _carouselController.SetMainImage(_gfxRepresentation);  
+    }
+}
