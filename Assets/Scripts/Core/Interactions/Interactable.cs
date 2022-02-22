648274266ec74636ee0b7334bf3b3769b6562bc8
zzosdunk <zzosdunk@gmail.com>
2022-02-22 17:11:57 +0100

    hiding item particle system

--- Assets/Scripts/Core/Interactions/Interactable.cs
+++ Assets/Scripts/Core/Interactions/Interactable.cs
@@ -8,22 +8,35 @@ namespace Core.Interactions
     {
         [SerializeField] private InventoryItem _item;
         [SerializeField] private List<Outline> _outline;
+
+        private bool _isInteracted;
         
         public virtual string InteractionMessage { get; }
 
         public virtual void Show()
         {
-            _outline.ForEach(o => o.enabled = true);
+            if (!_isInteracted)
+            {
+                _outline.ForEach(o => o.enabled = true);
+            }
+            else
+            {
+                _outline.ForEach(o => o.enabled = false);
+            }
+            
         }
 
         public virtual void Hide()
         {
+            _isInteracted = false;
             _outline.ForEach(o => o.enabled = false);
         }
 
         public virtual void Interaction()
         {
             GameManager.Instance.EventManager.ItemPick(_item.HobbyData);
+            _isInteracted = true;
+            _outline.ForEach(o => o.enabled = false);
             _item.HideInteractableItem();
         }
     }
