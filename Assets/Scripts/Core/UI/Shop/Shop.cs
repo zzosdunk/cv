70d84fe47ccaa8c1f4129b757f84cd09d0608295
zzosdunk <zzosdunk@gmail.com>
2022-03-02 00:00:10 +0100

    shop [WIP]

--- Assets/Scripts/Core/UI/Shop/Shop.cs
+++ Assets/Scripts/Core/UI/Shop/Shop.cs
@@ -1,11 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Shop : MonoBehaviour
 {
+    [SerializeField] private GameObject _shopPanel;
     [SerializeField] private GameObject _progressInfoPanel;
     [SerializeField] private GameObject _completedStarsPanel;
+
+    [SerializeField] private Animator _starsCompletedAnim;
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Start()
+    {
+        // _shopPanel.SetActive(false);
+    }
 
     public void UnlockShop()
     {
@@ -17,6 +31,8 @@ public class Shop : MonoBehaviour
     {
         _progressInfoPanel.SetActive(false);
         _completedStarsPanel.SetActive(true);
+        
+        _starsCompletedAnim.Play("stars_completed");
     }
 
     public void EnableShop()
